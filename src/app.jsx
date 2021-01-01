b1788e723dca6933f2a27b12e65d011687c8a145
Farah Khurrum <fakhurru@microsoft.com>
2020-12-31 19:40:39 -0800

    Adding support for image loader

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,15 @@
 import './app.scss'
+import img from './common/images/test.jpg'
 import React from 'react'
 
 const App = () => (
-    <div className="container">
-        <h1>Hello World, React!</h1>
+    <div>
+        <div className="container">
+            <h1>Hello World, React!</h1>
+        </div>
+        <div className="container">
+            <img className="image" src={img}/>
+        </div>
     </div>
 )
 
